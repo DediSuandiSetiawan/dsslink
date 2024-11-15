0d407b555441e05432a920a188beae5146e074b4
dedi-ss <100201967+dedi-ss@users.noreply.github.com>
2024-11-16 00:27:34 +0700

    Add files via upload

--- login/homepage.php
+++ login/homepage.php
@@ -4,16 +4,49 @@ include("connect.php");
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
+<title>Homepage</title>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">   
 </head>
+
 <body>
-    <div style="text-align:center; padding:15%;">
+ <!-- Carousel Start -->
+ <div class="container-fluid p-0 mb-5">
+        <div class="owl-carousel header-carousel position-relative">
+            <div class="owl-carousel-item position-relative">
+                <img class="img-fluid" src="dss-main-Git/img/1.jpg" alt="">
+                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
+                    style="background: rgba(24, 29, 56, .7);">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-sm-10 col-lg-8">
+                            <div style="text-align:center; padding:5%;">
+                                <h1 class="text-primary text-uppercase mb-3 animated slideInDown">Belajar Online Terbaik
+                                </h1>
+                                <h1 class="display-5 text-white animated slideInDown">DSS LINK CENTER
+                                </h1>
+                                <p class="fs-5 text-white mb-4 pb-2"> Belajar Dimana Saja, Bisa!!</p>
+                                <a href="http://localhost/dsslink-main-Git/index.html" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Baca
+                                    selengkapnya</a>
+                                <a href="" class="btn btn-light py-md-3 px-md-5 animated slideInRight">Gabung
+                                    Sekarang</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <!-- Carousel End -->
+         
+    <div style="text-align:center; padding:1%;">
       <p  style="font-size:50px; font-weight:bold;">
        Thanks <?php 
        if(isset($_SESSION['email'])){
@@ -24,6 +57,7 @@ include("connect.php");
         }
        }
        ?>
+       
        :)
       </p>
       <a href="logout.php">Logout</a>
@@ -33,98 +67,38 @@ include("connect.php");
   <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="container py-5">
         <div class="row g-5">
+        <div style="text-align:center; padding:10px;">
+        <p  style="font-size:10px; font-weight:bold;">
             <div class="col-lg-3 col-md-6">
-                <h4 class="text-white mb-3">Quick Link</h4>
-                <a class="btn btn-link" href="https://dedi-ss.github.io/dsslink/about.html">About</a>
-                <a class="btn btn-link" href="https://dedi-ss.github.io/dsslink/contact.html">Contact</a>
+                
+                <h1 class="text-white mb-3">Quick Link</h1>
+                <a class="btn btn-link" href="">About</a>
+                <a class="btn btn-link" href="">Contact</a>
                 <a class="btn btn-link" href="">Privacy Policy</a>
                 <a class="btn btn-link" href="">Terms & Condition</a>
                 <a class="btn btn-link" href="">FAQs & Help</a>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="text-white mb-3">Kontak</h4>
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Karawang, Indonesia</p>
-                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+62 812 9804 0103</p>
-                <p class="mb-2"><i class="fa fa-envelope me-3"></i>dsslinkcenter@gmail.com</p>
-                <div class="d-flex pt-2">
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="text-white mb-3">Gallery</h4>
-                <div class="row g-2 pt-2">
-                    <div class="col-4">
-                        <img class="img-fluid bg-light p-1" src="img/course-1.jpg" alt="">
-                    </div>
-                    <div class="col-4">
-                        <img class="img-fluid bg-light p-1" src="img/course-2.jpg" alt="">
-                    </div>
-                    <div class="col-4">
-                        <img class="img-fluid bg-light p-1" src="img/course-3.jpg" alt="">
-                    </div>
-                    <div class="col-4">
-                        <img class="img-fluid bg-light p-1" src="img/course-2.jpg" alt="">
-                    </div>
-                    <div class="col-4">
-                        <img class="img-fluid bg-light p-1" src="img/course-3.jpg" alt="">
-                    </div>
-                    <div class="col-4">
-                        <img class="img-fluid bg-light p-1" src="img/course-1.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="text-white mb-3">Newsletter</h4>
-                <p>Update terbaru dan konten eksklusif melalui newsletter kami.</p>
-                <div class="position-relative mx-auto" style="max-width: 400px;">
-                    <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                    <button type="button"
-                        class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-                </div>
-            </div>
+            </div>               
         </div>
     </div>
     <div class="container">
         <div class="copyright">
             <div class="row">
+                  <div style="text-align:center; padding:1%;">
+        <p  style="font-size:50px; font-weight:bold;">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
                     &copy; <a class="border-bottom" href="#">All Right Reserved.</a>
-
                     <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
                     Designed By <a class="border-bottom" href="https://htmlcodex.com">dss link center 2024</a>
                 </div>
-                <div class="col-md-6 text-center text-md-end">
-                    <div class="footer-menu">
-                        <a href="">Home</a>
-                        <a href="">Cookies</a>
-                        <a href="">Help</a>
-                        <a href="">FQAs</a>
-                    </div>
-                </div>
-            </div>
+             </div>
         </div>
     </div>
 </div>
 <!-- Footer End -->
 
 
-<!-- Back to Top -->
-<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-
-
-<!-- JavaScript Libraries -->
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="lib/wow/wow.min.js"></script>
-<script src="lib/easing/easing.min.js"></script>
-<script src="lib/waypoints/waypoints.min.js"></script>
-<script src="lib/owlcarousel/owl.carousel.min.js"></script>
-
 <!-- Template Javascript -->
 <script src="js/main.js"></script>
 </body>
-
 </html>
